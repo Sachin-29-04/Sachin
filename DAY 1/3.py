0d0c7038ce36048c173e1eae736f17bed59cd141
Sachin Sahu <ss.sachinsahu.294@gmail.com>
2025-06-23 21:04:49 +0530

    2 more program of Assessment 1

---
+++ DAY 1/3.py
@@ -0,0 +1,22 @@
+# (3) Use print function to print strings
+
+city="Jaipur"
+state="Rajasthan"
+country="India"
+
+print("City : " ,city , "  State : ", state , "  Country : ",country)
+print("City : " ,city , "  State : ", state , "  Country : ",country,sep="----")
+print("City : " ,city , "  State : ", state , "  Country : ",country)
+
+
+print("hello python" ,"welcome",6,sep="...",end=" --- ")
+
+print("hello",end="  777  ")
+
+print("hiii")
+
+print(4567)
+
+print(10<5)
+
+print(5<10)
---
+++ DAY 1/4.py
@@ -0,0 +1,25 @@
+
+n1=int(input("enter any number "))
+
+oper=input("enter operator")
+
+n2=int(input("enter any number "))
+
+
+
+
+if oper=="+":
+    print("Addition of n1 and n2 is ",n1+n2)
+
+elif oper=="-" :
+    print("Subtraction of n1 and n2 is ",n1-n2)
+
+elif oper=="*"  :
+    print("Multiplication of n1 and n2 is ",n1*n2)
+
+elif oper=="/" :
+    print("Division of n1 and n2 is ",n1/n2)
+
+
+
+
